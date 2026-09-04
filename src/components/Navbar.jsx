36e7f677a9cc2fde23ef0b12163758a01527bcd5
harryinnotech <134111114+harryinnotech@users.noreply.github.com>
2026-09-04 20:50:34 +0100

    Optimize website logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import logo from "../assets/logo.png";
+import logo from "../assets/logo.webp";
 export default function Navbar() {
   const [open, setOpen] = useState(false);
 
